2a5c402271ec0df8756ce6327b0f80dc2d8438fd
AidenRacelis <aidenracelis@gmail.com>
2026-06-23 00:51:34 -0400

    add photos in about me

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-function toggleMenu(){
-    const menu= document.querySelector(".menu-links");
-    const icon= document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
-function toggleMenu(){
-    const menu= document.querySelector(".menu-links");
-    const icon= document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+
+const aboutGalleryImage = document.getElementById("about-gallery-image");
+
+if (aboutGalleryImage) {
+    const aboutGalleryPhotos = [
+        "./assets/about_me/about.png",
+        "./assets/about_me/fam_1.JPG",
+        "./assets/about_me/fam_2.JPG"
+    ];
+
+    let currentAboutPhotoIndex = 0;
+
+    const updateAboutGalleryImage = () => {
+        aboutGalleryImage.src = aboutGalleryPhotos[currentAboutPhotoIndex];
+        aboutGalleryImage.alt = `Aiden Racelis about photo ${currentAboutPhotoIndex + 1}`;
+    };
+
+    const showNextAboutPhoto = () => {
+        currentAboutPhotoIndex =
+            (currentAboutPhotoIndex + 1) % aboutGalleryPhotos.length;
+        updateAboutGalleryImage();
+    };
+
+    const showPreviousAboutPhoto = () => {
+        currentAboutPhotoIndex =
+            (currentAboutPhotoIndex - 1 + aboutGalleryPhotos.length) %
+            aboutGalleryPhotos.length;
+        updateAboutGalleryImage();
+    };
+
+    document
+        .querySelector(".about-gallery__btn--next")
+        ?.addEventListener("click", showNextAboutPhoto);
+
+    document
+        .querySelector(".about-gallery__btn--prev")
+        ?.addEventListener("click", showPreviousAboutPhoto);
+
+    updateAboutGalleryImage();
 }
